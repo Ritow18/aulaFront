cbb6afd99aca7a02a025eaad85d1dbbaa1e8b1ec
Ritow18 <Ristowedson24@gmail.com>
2025-05-18 00:26:29 -0300

    Update form.js
    javascript que estava no html, agora em um arquivo separado

--- port_front_aula/form/form.js
+++ port_front_aula/form/form.js
@@ -1,1 +1,38 @@
+class Contato {
+    constructor(){
+        this.Nome = null
+        this.email = null
+        this.phone = null
+        this.mensagem = null
+        this.dt_cad = Date
+        this.idade = null
+        this.tipo_contato = null
+    }
+    //método especifico da classe contato
+    Enviar(){
+        let usuario = " aaaasassdaddsadadasdasdas"; 
+        let ano = 2025;
+        const aviso = "você está na pagina de contato";
 
+        this.Nome = document.getElementById('Nome').value;
+        this.email = null
+        this.phone = null
+        this.mensagem = null
+        this.dt_cad = Date
+        this.idade = null
+        this.tipo_contato = null
+    
+        // => "=" atribui   =>"=="compara o conteudo string a =2025; int b = 2025  a==b(true) => "===" compara variaveis 
+        // string a = 2025 int b = 2025 a===b(false)
+
+        if(usuario === ano){
+            alert("dados invalidos")
+        }
+        if(this.Nome===''){
+            alert("a area de nome precisa ser preenchida!")
+        }else{
+        alert(aviso + "de" + this.Nome+" em "+ this.dt_cad());
+        }
+    }
+}
+contato = new Contato();
